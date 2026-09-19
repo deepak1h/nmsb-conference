70b0fa05281883d18b3dbe7000a441453249455b
deepak1h <deepakkumarpandeychs@gmail.com>
2026-09-19 22:58:13 +0530

    feat: add Navbar component and conference configuration variables

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,9 +20,6 @@ export default function Navbar() {
       <div className="container">
         {/* Agora Flower Logo */}
         <Link href="/" className="agora-logo-container" onClick={closeMobileMenu}>
-          <svg className="agora-flower-icon" viewBox="0 0 40 40" fill="currentColor">
-            <path d="M20 5C22.2 5 24 6.8 24 9C24 11.2 22.2 13 20 13C17.8 13 16 11.2 16 9C16 6.8 17.8 5 20 5ZM31 16C33.2 16 35 17.8 35 20C35 22.2 33.2 24 31 24C28.8 24 27 22.2 27 20C27 17.8 28.8 16 31 16ZM31 31C33.2 31 35 32.8 35 35C35 37.2 33.2 39 31 39C28.8 39 27 37.2 27 35C27 32.8 28.8 31 31 31ZM20 27C22.2 27 24 28.8 24 31C24 33.2 22.2 35 20 35C17.8 35 16 33.2 16 31C16 28.8 17.8 27 20 27ZM9 16C11.2 16 13 17.8 13 20C13 22.2 11.2 24 9 24C6.8 24 5 22.2 5 20C5 17.8 6.8 16 9 16ZM9 31C11.2 31 13 32.8 13 35C13 37.2 11.2 39 9 39C6.8 39 5 37.2 5 35C5 32.8 6.8 31 9 31Z" />
-          </svg>
           <span className="agora-logo-text">{config.conference.shortName}</span>
         </Link>
 
